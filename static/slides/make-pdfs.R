1058070336261c31d57bcc7e9d9b706de81dc30a
Ryan Safner <ryansafner@gmail.com>
2021-09-02 22:00:44 -0400

    PS 1

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,11 +1,11 @@
 # make-pdfs
-pagedown::chrome_print(input = "https://blissful-euler-0e9a9d.netlify.app/slides/1.2-slides.html",
-                       output = "static/slides/1.2-slides.pdf",
+pagedown::chrome_print(input = "https://blissful-euler-0e9a9d.netlify.app/slides/1.4-slides.html",
+                       output = "static/slides/1.4-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://blissful-euler-0e9a9d.netlify.app/slides/1.2-slides.html",
-                       output = "static/slides/1.2-slides.png",
+pagedown::chrome_print(input = "https://blissful-euler-0e9a9d.netlify.app/slides/1.4-slides.html",
+                       output = "static/slides/1.4-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
