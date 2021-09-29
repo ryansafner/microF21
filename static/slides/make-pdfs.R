43fedc75f495dab1030d42792df4a5dfa0ce656e
Ryan Safner <ryansafner@gmail.com>
2021-09-28 22:44:57 -0400

    2.1 pdf

--- static/slides/make-pdfs.R
+++ static/slides/make-pdfs.R
@@ -1,11 +1,11 @@
 # make-pdfs
-pagedown::chrome_print(input = "https://blissful-euler-0e9a9d.netlify.app/slides/1.6-slides.html",
-                       output = "static/slides/1.6-slides.pdf",
+pagedown::chrome_print(input = "https://microF21.classes.ryansafner.com/slides/2.1-slides.html",
+                       output = "static/slides/2.1-slides.pdf",
                        timeout = 300)
 
 # create png of first slide
-pagedown::chrome_print(input = "https://blissful-euler-0e9a9d.netlify.app/slides/1.6-slides.html",
-                       output = "static/slides/1.6-slides.png",
+pagedown::chrome_print(input = "https://microF21.classes.ryansafner.com/slides/2.1-slides.html",
+                       output = "static/slides/2.1-slides.png",
                        selector = "div.remark-slide",
                        format = "png",
                        timeout = 300)
